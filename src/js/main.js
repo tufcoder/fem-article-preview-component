39e212d3a12830bbbb9d5684fed74b50f0f47031
Oswaldo Castro <oswaldogpc@gmail.com>
2024-04-21 17:13:44 -0300

    finished desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,13 @@ const avatarShare = document.querySelector('.avatar-share')
 const buttonShare = document.querySelector('.button-share')
 
 buttonShare.addEventListener('click', (event) => {
-    avatar.classList.toggle('share');
-    avatarInfo.classList.toggle('hidden')
-    avatarShare.classList.toggle('hidden')
+    if (document.body.clientWidth < 1440) {
+        avatar.classList.toggle('share');
+        avatarInfo.classList.toggle('hidden')
+        avatarShare.classList.toggle('hidden')
+    } else {
+        avatarShare.classList.toggle('hidden')
+        avatarShare.classList.toggle('share')
+    }
     event.currentTarget.classList.toggle('button-share-active')
 })
